28248e69278756281e79fbcf758253fd13c4e4c4
timo.schoemaker <timo.schoemaker@hotmail.de>
2022-09-09 14:13:46 +0200

    finnished day 2 (but needs too long)

--- nr17.cpp
+++ nr17.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
 #include<fstream>
 #include <cstdio>
+#include <execution>
+#include<atomic>
+#include <algorithm>
+#include<vector>
 struct Rect{
     int xMin,xMax,yMin,yMax;
     Rect(int xMin, int xMax, int yMin, int yMax):xMin(xMin),xMax(xMax),yMin(yMin),yMax(yMax){
@@ -55,22 +59,34 @@ int main(int argc,char** argv){
     //printf("%d %d %d %d \n",rect.xMin,rect.xMax,rect.yMin,rect.yMax);
     //std::cout<<getMinXSpeed(rect.xMin)<<std::endl;
 
-
+    bool task2=argc >2;
+    constexpr  int FACTOR=300;
     int minDX=getMinXSpeed(rect.xMin);
-    int maxDX=getMinXSpeed(3*rect.xMax);
+    int maxDX=getMinXSpeed(FACTOR*rect.xMax);
 
-    int minDY=-std::max(abs(rect.yMin),abs(rect.yMax));
-    int maxDY=-minDY;
-
-    int maxYVal=0;
+    int minDY= -std::max(abs(rect.yMin),abs(rect.yMax));
+    int maxDY=FACTOR*-minDY;
+    std::atomic<int> counter=0;
+    std::atomic<int> maxYVal;
+    std::vector<std::pair<int,int>>speeds;
     for(int dy=minDY;dy<=maxDY;dy++){
         for(int dx=minDX;dx<=maxDX;dx++){
-             bool b =simulate(dx,dy,maxYVal,rect);
-             if(b){
-                //std::cout<<dx<< " "<<dy << " " <<maxYVal<< std::endl;
-             }
+            speeds.push_back(std::make_pair(dx,dy));
         }
     }
-    std::cout<<maxYVal<<std::endl;
+    std::cout<<"all created"<<std::endl;
+    std::for_each(std::execution::par_unseq,speeds.begin(),speeds.end(),[&]( std::pair<int,int>& p){
+        int maxY;
+        bool b=simulate(p.first,p.second,maxY,rect);
+        if(b){
+            maxYVal=std::max(maxY,int(maxYVal));
+            counter++;
+        }
+    });
+    if(task2)
+        std::cout<<counter<<std::endl;
+    else
+        std::cout<<maxYVal<<std::endl;
+
 
 }
